41901e2d340f2fd923078eba88bdd4d6ba2e8abf
Balalarga <lightman03@mail.ru>
2023-09-26 15:23:08 +0300

    Cleanup

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,7 +2,9 @@
 #include "imgui-SFML.h"
 
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
@@ -13,9 +15,26 @@ int main()
     window.setFramerateLimit(60);
     if (!ImGui::SFML::Init(window))
         return -1;
-
+    
+    sf::Texture texture;
+    texture.create(window.getSize().x, window.getSize().y);
+    sf::Image textureImage;
+    textureImage.create(window.getSize().x, window.getSize().y);
+    for (unsigned i = 0; i < window.getSize().x; ++i)
+    {
+        for (unsigned j = 0; j < window.getSize().y; ++j)
+        {
+            textureImage.setPixel(i, j, sf::Color(80, 40, 40));
+        }
+    }
+    
+    texture.loadFromImage(textureImage);
+  
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+  
+    sf::Sprite background(texture);
+    // sprite.setPosition(200, 200);
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -35,6 +54,7 @@ int main()
         ImGui::End();
 
         window.clear();
+        window.draw(background);
         window.draw(shape);
         ImGui::SFML::Render(window);
         window.display();
