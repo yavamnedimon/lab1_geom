6878a6d466cd09f87c907a0ea4645c5736e2e92f
Balalarga <lightman03@mail.ru>
2023-09-26 17:56:02 +0300

    Make RSprite

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,64 +1,67 @@
+#include <iostream>
+
 #include "imgui.h"
 #include "imgui-SFML.h"
+#include "Graphics/RFSprite.h"
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/RenderTexture.hpp"
+#include "SFML/System/Clock.hpp"
+#include "SFML/Window/Event.hpp"
 
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/Sprite.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/RenderTexture.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
+
+float CircleFunc(sf::Vector2f spacePos)
+{
+    return std::powf(1, 2) - std::powf(spacePos.x, 2) - std::powf(spacePos.y, 2);
+}
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Geometry modeling 1");
     window.setFramerateLimit(60);
     if (!ImGui::SFML::Init(window))
-        return -1;
-    
-    sf::Texture texture;
-    texture.create(window.getSize().x, window.getSize().y);
-    sf::Image textureImage;
-    textureImage.create(window.getSize().x, window.getSize().y);
-    for (unsigned i = 0; i < window.getSize().x; ++i)
     {
-        for (unsigned j = 0; j < window.getSize().y; ++j)
-        {
-            textureImage.setPixel(i, j, sf::Color(80, 40, 40));
-        }
+        std::cout << "ImGui initialization failed\n";
+        return -1;
     }
     
-    texture.loadFromImage(textureImage);
-  
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-  
-    sf::Sprite background(texture);
-    // sprite.setPosition(200, 200);
+    RFSprite functionCanvas(window.getSize());
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
+		static sf::Clock deltaClock;
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             ImGui::SFML::ProcessEvent(window, event);
 
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Button::Right)
+            {
+            	functionCanvas.Update(&CircleFunc);
             }
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
+        // ImGui::Begin("Hello, world!");
+        // ImGui::Button("Look at this pretty button");
+        // ImGui::End();
 
         window.clear();
-        window.draw(background);
-        window.draw(shape);
+        window.draw(functionCanvas);
+      
         ImGui::SFML::Render(window);
         window.display();
     }
-
     ImGui::SFML::Shutdown();
+
+
+	functionCanvas.SaveTo("Canvas.png");
+	
+	return 0;
 }
