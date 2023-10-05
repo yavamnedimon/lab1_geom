b5d79b622f4456dc2395f8998f58d502c2e48410
Balalarga <lightman03@mail.ru>
2023-10-06 00:28:21 +0300

    Reorganize code

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,6 +8,40 @@
 #include "SFML/Window/Event.hpp"
 
 
+void HandleUserInput(sf::RenderWindow& window, const sf::Event& event)
+{
+	switch (event.type)
+	{
+	case sf::Event::Closed:
+		window.close();
+		break;
+	default:
+		break;
+	}
+}
+
+void Update(sf::RenderWindow& window, const sf::Time& deltaClock)
+{
+	// Make some time-dependent updates, like: physics, gameplay logic, animations, etc.
+}
+
+void Render(sf::RenderWindow& window)
+{
+	
+}
+
+void RenderGui(sf::RenderWindow& window)
+{
+	ImGui::Begin("Hello, world!");
+		
+	if (ImGui::Button("Look at this pretty button"))
+		std::cout << "Button pressed\n";
+    	
+	ImGui::Text("Look at this pretty text");
+	ImGui::End();
+}
+
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Geometry modeling 1");
@@ -18,31 +52,25 @@ int main()
         return -1;
     }
 	
+	sf::Clock deltaClock;
     while (window.isOpen())
     {
-		static sf::Clock deltaClock;
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(window, event);
-			
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-        ImGui::SFML::Update(window, deltaClock.restart());
+		sf::Event event;
+    	while (window.pollEvent(event))
+    	{
+    		ImGui::SFML::ProcessEvent(window, event);
+    		HandleUserInput(window, event);
+    	}
+    	
+    	sf::Time deltaTime = deltaClock.restart();
+        ImGui::SFML::Update(window, deltaTime);
+    	Update(window, deltaTime);
     	
     	window.clear();
-		
-        ImGui::Begin("Hello, world!");
-		
-    	if (ImGui::Button("Look at this pretty button"))
-    		std::cout << "Button pressed\n";
     	
-		ImGui::Text("Look at this pretty text");
-        ImGui::End();
+    	Render(window);
     	
+    	RenderGui(window);
         ImGui::SFML::Render(window);
     	
         window.display();
