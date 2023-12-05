a1f78d855e2e1c914210b93206254931f6582ed4
Дмитрий Караганов <dmitrijkaraganov76@gmail.com>
2023-12-05 16:12:47 +0300

    Initial

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,63 +1,29 @@
 #include <iostream>
-
-#include "imgui.h"
-#include "imgui-SFML.h"
 #include "SFML/Graphics/RenderWindow.hpp"
-#include "SFML/Graphics/Text.hpp"
-#include "SFML/Graphics/Image.hpp"
-#include "SFML/Graphics/Texture.hpp"
-#include "SFML/Graphics/Sprite.hpp"
-#include "SFML/Graphics/CircleShape.hpp"
-#include "SFML/Graphics/RenderTexture.hpp"
 #include "SFML/System/Clock.hpp"
 #include "SFML/Window/Event.hpp"
+#include "imgui-SFML.h"
+#include "imgui.h"
 
-
-void HandleUserInput(sf::RenderWindow& window, const sf::Event& event)
+void UpdateBackgroundColor(sf::Color &backgroundColor, const sf::Time &deltaTime)
 {
-	switch (event.type)
-	{
-	case sf::Event::Closed:
-		window.close();
-		break;
-	default:
-		break;
-	}
-}
+	
+	float colorChangeSpeed = 70.0f;
 
-void Update(sf::RenderWindow& window, const sf::Time& deltaClock)
-{
-	// Make some time-dependent updates, like: physics, gameplay logic, animations, etc.
+	
+	backgroundColor.r =
+		static_cast<sf::Uint8>(std::min(255.f, backgroundColor.r + colorChangeSpeed * deltaTime.asSeconds()));
+	backgroundColor.g =
+		static_cast<sf::Uint8>(std::min(255.f, backgroundColor.g + colorChangeSpeed * deltaTime.asSeconds()));
+	backgroundColor.b =
+		static_cast<sf::Uint8>(std::min(255.f, backgroundColor.b + colorChangeSpeed * deltaTime.asSeconds()));
+	std::cout << "R: " << static_cast<int>(backgroundColor.r) << " G: " << static_cast<int>(backgroundColor.g)
+			  << " B: " << static_cast<int>(backgroundColor.b) << std::endl;
 }
-sf::Sprite sprite;
-sf::Texture spriteTexture;
-sf::Image spriteImage;
-
-void Render(sf::RenderWindow& window)
-{
-	// Draw some sfml/opengl items
-	static sf::CircleShape circle = []()
-	{
-		sf::CircleShape circle;
-		circle.setRadius(50);
-		circle.setPosition(200, 200);
-		circle.setFillColor(sf::Color::White);
-		return circle;
-	}();
-	window.draw(circle);
-	window.draw(sprite);
-}
-
-void RenderGui(sf::RenderWindow& window)
-{
-	ImGui::Begin("Default window");
-	ImGui::End();
-}
-
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Geometry modeling 1");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "changing the background");
 	window.setFramerateLimit(60);
 	if (!ImGui::SFML::Init(window))
 	{
@@ -65,37 +31,24 @@ int main()
 		return -1;
 	}
 
-	spriteImage.create(200, 200);
-	for (int x = 0; x < spriteImage.getSize().x; ++x)
-	{
-		for (int y = 0; y < spriteImage.getSize().y; ++y)
-		{
-			spriteImage.setPixel(x, y, sf::Color(rand()%255, rand()%255, rand()%255));
-		}	
-	}
-
-	spriteTexture.loadFromImage(spriteImage);
-	sprite.setTexture(spriteTexture);
-	sprite.setPosition(400, 400);
-
 	sf::Clock deltaClock;
+	sf::Color background_color = sf::Color::Red; // Начальный цвет фона
+
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			ImGui::SFML::ProcessEvent(window, event);
-			HandleUserInput(window, event);
+			if (event.type == sf::Event::Closed)
+				window.close();
 		}
 
 		sf::Time deltaTime = deltaClock.restart();
 		ImGui::SFML::Update(window, deltaTime);
-		Update(window, deltaTime);
 
-		window.clear();
-
-		RenderGui(window);
-		Render(window);
+		UpdateBackgroundColor(background_color, deltaTime);
+		window.clear(background_color); // Устанавливаем цвет фона
 
 		ImGui::SFML::Render(window);
 
